8c3f482583379632e86fefd780cc1c3db68899d1
jfriedlein <johannes.friedlein@fau.de>
2020-08-27 10:58:21 +0200

    added safety to avoid sqrt of a negative number

--- elasto_plasticity_modules/get_yielding_norm.f
+++ elasto_plasticity_modules/get_yielding_norm.f
@@ -6,9 +6,23 @@ c
       use Tensor
       !implicit none
 c
-      type(Tensor2) :: stress, tmp
+      type(Tensor2) :: stress
       type(Tensor4) :: HillT_H
+      double precision tmp
 c
-      get_yielding_norm = sqrt (stress .ddot. HillT_H .ddot. stress )
+      tmp = stress .ddot. HillT_H .ddot. stress
+c In case the S:H:S expression gets too negative, we catch this to kick an 
+c error message.
+      if ( tmp < -1e-20 ) then
+          write(*,*) "Yielding norm S:H:S got a bit too negative: ",tmp,
+     &               ". Check this case and maybe increase the
+     & tolerance for this error."
+          pause
+c Tiny negative numbers are okay, but we still need positive numbers for the
+c square root, to avoid getting positive yielding norms even for nonsense
+c S:H:S values, we limit the value to at least zero (max(...,0))
+      else
+          get_yielding_norm = sqrt( max(tmp,0.) )
+      endif     
 c      
       end function get_yielding_norm
