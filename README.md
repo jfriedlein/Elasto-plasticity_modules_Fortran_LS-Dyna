2f05d809858ca526a71044e4120096af09b72dc4
jfriedlein <johannes.friedlein@fau.de>
2020-09-04 08:27:32 +0200

    Finally qconv!

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # Elasto-plasticity_modules_LS-Dyna
 A module containing elasto-plastic material models (Hill-Plasticity) with straightforward extension to various hardening laws. 
 
+(also available for C++ library deal.II [here](https://github.com/jfriedlein/Elasto-plasticity_modules_dealii))
+
+## What it does
+We offer a framework to capture elasto-plastic material models up to anisotropic Hill-plasticity with various hardening laws. The framework can be found in the exemplary `MaterialModel.h` file and also contains subiterations on the qp level. Different hardenig laws defined by the hardening stress `R` and an evolution equation for the internal hardening variable `alpha` can be defined by only three equations (R, alpha, d_R_d_gamma in `elpl_equation_list.h`). The algorithm is general enough to produce quadratic convergence for any such defined hardening law. Currently, only isotropic hardening is supported, an extension for kinematic hardening might follow in the future.
+
+@todo note on low efficiency, recommended for testing
+
+@todo add the enumerator_list or a section of it
+
+@todo ensure that it is standalone
+
+@todo add support for Sacado (for arbitrary evolution equations, etc)
+
+@todo add a verification example for anisotropy (either from external source for own verifi, or plate with a hole for internal verifi)
 
 DOCU still missing!!!
--- elasto_plasticity_modules/get_dPhi_dgamma.f
+++ elasto_plasticity_modules/get_dPhi_dgamma.f
@@ -2,16 +2,16 @@ c
 c
 c
       double precision function get_dPhi_dgamma( alpha_k, HillT_H, n_n1,
-     & gamma_k, hardening_type, cm, hsv )
+     & stress_t, gamma_k, hardening_type, cm, hsv )
 c
       use Tensor
       use TensorXkinematics
       use cm_manager
       !implicit none
 c
-      type(Tensor4) :: HillT_H, A_inv
-      type(Tensor2) :: Eye, n_n1
-      double precision alpha_k, gamma_k
+      type(Tensor4) :: HillT_H, A_inv, d_Ainv_dgamma
+      type(Tensor2) :: Eye, n_n1, stress_t
+      double precision alpha_k, gamma_k, d_R_d_gamma
       double precision hardStress_R, yield_stress, shearMod_mu
       dimension cm(*), hsv(*)
       integer :: hardening_type
@@ -21,17 +21,24 @@ c Material parameters
 c Second order identity tensor
       Eye = identity2(Eye)
 c      
+      d_R_d_gamma = get_d_R_d_gamma( alpha_k, gamma_k, 
+     &                               hardening_type, cm, hsv )
+c
       hardStress_R = get_hardeningStress_R(alpha_k, hardening_type, cm)
+c
       A_inv = inv( identity4(Eye)
      &                 + (2. * shearMod_mu * gamma_k
      &                  / ( sqrt(2./3.) * (yield_stress-hardStress_R) ))
      &                  * HillT_H )
 c
-c @todo Check why we get factors of 2 on the main diagonal for A_inv = inv(identity) instead of 0.5 as in DII
-	! @todo The following only achieves superlinear behaviour close to the solution, before it's quadratic
-	get_dPhi_dgamma = - 2. * shearMod_mu * (n_n1 .ddot. A_inv .ddot. n_n1)
-     &                  + sqrt( 2./3. )
-     &                    * get_d_R_d_gamma( alpha_k, gamma_k, 
-     &                                       hardening_type, cm, hsv )
+      d_Ainv_dgamma
+     &    = (A_inv .ddot. A_inv)
+     &      .ddot. HillT_H
+     &      * ( -2. * shearMod_mu )
+     &           / ( sqrt(2./3.)*(yield_stress-hardStress_R) )
+     &      * (1. + gamma_k/( yield_stress-hardStress_R) * d_R_d_gamma)
+c @note Be aware of the required parentheses here:
+      get_dPhi_dgamma = ( (n_n1 .ddot. d_Ainv_dgamma) .ddot. stress_t )
+     &                  + sqrt(2./3.) * d_R_d_gamma
 c      
       end function get_dPhi_dgamma
