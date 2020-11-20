e755de55a782c0c02806bbbecd6f1120a6c8682a
jfriedlein <johannes.friedlein@fau.de>
2020-11-20 16:43:40 +0100

    added Phi_k to tangents, but not yet tested

--- elasto_plasticity_modules/get_dPhi_dgamma.f
+++ elasto_plasticity_modules/get_dPhi_dgamma.f
@@ -2,7 +2,7 @@ c
 c
 c
       double precision function get_dPhi_dgamma( alpha_k, HillT_H, n_n1,
-     & stress_t, gamma_k, hardening_type, cm, hsv )
+     & stress_t, gamma_k, Phi_k, hardening_type, cm, hsv )
 c
       use Tensor
       use TensorXkinematics
@@ -11,7 +11,7 @@ c
 c
       type(Tensor4) :: HillT_H, A_inv, d_Ainv_dgamma
       type(Tensor2) :: Eye, n_n1, stress_t
-      double precision alpha_k, gamma_k, d_R_d_gamma
+      double precision alpha_k, gamma_k, d_R_d_gamma, Phi_k
       double precision hardStress_R, yield_stress, shearMod_mu
       dimension cm(*), hsv(*)
       integer :: hardening_type
@@ -35,8 +35,10 @@ c
      &    = (A_inv .ddot. A_inv)
      &      .ddot. HillT_H
      &      * ( -2. * shearMod_mu )
-     &           / ( sqrt(2./3.)*(yield_stress-hardStress_R) )
-     &      * (1. + gamma_k/( yield_stress-hardStress_R) * d_R_d_gamma)
+     &           / ( sqrt(2./3.)*(yield_stress-hardStress_R) + Phi_k )
+     &      * (1. + gamma_k/( sqrt(1.5)*Phi_k
+     &                        + (yield_stress-hardStress_R) )
+     &              * d_R_d_gamma)
 c @note Be aware of the required parentheses here:
       get_dPhi_dgamma = ( (n_n1 .ddot. d_Ainv_dgamma) .ddot. stress_t )
      &                  + sqrt(2./3.) * d_R_d_gamma
