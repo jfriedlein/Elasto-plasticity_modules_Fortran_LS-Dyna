184b4bc967105a27801c81608d7077107479fe9e
jfriedlein <Johannes.Friedlein@fau.de>
2021-10-08 16:01:22 +0200

    played with the data types (WIP), using the enumerator for the hardening

--- elasto_plasticity_modules/get_d_R_d_gamma.f
+++ elasto_plasticity_modules/get_d_R_d_gamma.f
@@ -10,15 +10,16 @@ c
       use hsv_manager
       !implicit none
 c
-      double precision gamma_k, alpha_k
-      dimension cm(*), hsv(*)
+      real gamma_k
+      real(kind=8) alpha_k
+      real, dimension (*) :: cm, hsv
       integer :: hardening_type
 c Material parameters
       select case( hardening_type )
-        case( 0 ) ! linear hardening
+        case( enum_hardening_linear ) ! linear hardening
          get_d_R_d_gamma = - cm_get('hardMod_K_______',cm)
      &                       * sqrt(2./3.)
-        case( 1 ) ! saturated alpha
+        case( enum_hardening_saturatedAlpha ) ! saturated alpha
          get_d_R_d_gamma =  - cm_get('hardMod_K_______',cm)
      &                      * ( sqrt(2./3.)
      &						* (1. - cm_get('hardMod_K_______',cm)
@@ -26,24 +27,24 @@ c Material parameters
      &                          * hsv_get_scalar('alpha', hsv)) ! to get alpha_n
      &					 	/ (1. + sqrt(2./3.) * cm_get('hardMod_K_______',cm)
      &                 /cm_get('hardStress_R_inf',cm) * gamma_k)**2 )
-        case( 2 ) ! saturated_Voce_hard_stress
+        case( enum_hardening_Voce ) ! saturated_Voce_hard_stress
          get_d_R_d_gamma = - cm_get('hardMod_K_______',cm)
      &                        * exp( -cm_get('hardMod_K_______',cm)
      &                        /cm_get('hardStress_R_inf',cm) * alpha_k )
      &                        * sqrt(2./3.)
-        case( 3 ) ! saturated_Miehe_hard_stress
+        case( enum_hardening_linExp ) ! saturated_Miehe_hard_stress
          get_d_R_d_gamma = (- cm_get('hardMod_K_______',cm)
      &                    - cm_get('hardStress_R_inf',cm)
      &                      * cm_get('hardMod_K_exp___',cm)
      &                    * exp( -cm_get('hardMod_K_exp___',cm)
      &                           * alpha_k ))
      &                    * sqrt(2./3.)
-        case( 4 ) ! Swift exponential hardening equations as in LS-Dyna (Manual Vol.II, *MAT_122_3D)
+        case( enum_hardening_Swift ) ! Swift exponential hardening equations as in LS-Dyna (Manual Vol.II, *MAT_122_3D)
          get_d_R_d_gamma = (-sqrt(2./3.)) *cm_get('hardMod_K_______',cm)
      &                     * cm_get('hardMod_K_exp___',cm)
      &                     * ( alpha_k+0.01 )
      &                     ** (cm_get('hardMod_K_exp___',cm)-1)
-        case( 5 ) ! exponent and exponential
+        case( enum_hardening_potExp ) ! exponent and exponential
          get_d_R_d_gamma = (
      &                        - cm_get('hardMod_K_______',cm)
      &                          * cm_get('hardExponent_n__',cm)
