b2e616aab8eb78ecd1ab5632c2e24d46337782f7
jfriedlein <Johannes.Friedlein@fau.de>
2021-10-08 16:01:49 +0200

    played with the data types (WIP), using the enumerator for the hardening

--- elasto_plasticity_modules/get_hardeningStress_R.f
+++ elasto_plasticity_modules/get_hardeningStress_R.f
@@ -8,33 +8,33 @@ c
       use cm_manager
       !implicit none
 c
-      double precision alpha
+      real(kind=8) alpha
       dimension cm(*)
       integer :: hardening_type
 c Material parameters
       select case( hardening_type )
-        case( 0 ) ! linear hardening
+        case( enum_hardening_linear ) ! linear hardening
          get_hardeningStress_R = - cm_get('hardMod_K_______',cm) * alpha
-        case( 1 ) ! saturated alpha
+        case( enum_hardening_saturatedAlpha ) ! saturated alpha
          get_hardeningStress_R = - cm_get('hardMod_K_______',cm) * alpha
-        case( 2 ) ! saturated_Voce_hard_stress
+        case( enum_hardening_Voce ) ! saturated_Voce_hard_stress
          get_hardeningStress_R = - cm_get('hardStress_R_inf',cm)
      &                       * ( 1
      &                            - exp(- cm_get('hardMod_K_______',cm)
      &                                    /cm_get('hardStress_R_inf',cm)
      &                                  * alpha) )
-        case( 3 ) ! saturated_Miehe_hard_stress
+        case( enum_hardening_linExp ) ! saturated_Miehe_hard_stress
          get_hardeningStress_R = - cm_get('hardMod_K_______',cm) * alpha
      &                           - cm_get('hardStress_R_inf',cm)
      &                         * ( 1
      &                             - exp(-cm_get('hardMod_K_exp___',cm)
      &                               * alpha) )
-        case( 4 ) ! Swift exponential hardening equations as in LS-Dyna (Manual Vol.II, *MAT_122_3D)
+        case( enum_hardening_Swift ) ! Swift exponential hardening equations as in LS-Dyna (Manual Vol.II, *MAT_122_3D)
           get_hardeningStress_R = cm_get('yieldStress_____',cm)
      &                            - cm_get('hardMod_K_______',cm)
      &                              * ( alpha+0.01 )
      &                                **cm_get('hardMod_K_exp___',cm)
-        case( 5 ) ! exponent and exponential
+        case( enum_hardening_potExp ) ! exponent and exponential
          get_hardeningStress_R = -cm_get('hardMod_K_______',cm)
      &                            * ( alpha+1e-3 )
      &                              **cm_get('hardExponent_n__',cm)
