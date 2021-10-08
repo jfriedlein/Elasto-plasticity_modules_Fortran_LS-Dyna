b81fe5aee46d06ed640506c01c0846b163cf1847
jfriedlein <Johannes.Friedlein@fau.de>
2021-10-08 16:01:56 +0200

    played with the data types (WIP), using the enumerator for the hardening

--- elasto_plasticity_modules/get_intVar_alpha.f
+++ elasto_plasticity_modules/get_intVar_alpha.f
@@ -8,25 +8,26 @@ c
       use cm_manager
       !implicit none
 c
-      double precision alpha_n, gamma_k
+      double precision alpha_n
+      real gamma_k
       dimension cm(*)
       integer :: hardening_type
 c Material parameters
       select case( hardening_type )
-        case( 0 ) ! linear hardening
+        case( enum_hardening_linear ) ! linear hardening
          get_intVar_alpha = alpha_n + sqrt(2./3.) * gamma_k
-        case( 1 ) ! saturated alpha
+        case( enum_hardening_saturatedAlpha ) ! saturated alpha
          get_intVar_alpha = (alpha_n + sqrt(2./3.) * gamma_k)
      &                     / ( 1. + sqrt(2./3.)
      &                         * cm_get('hardMod_K_______',cm)
      &                         /cm_get('hardStress_R_inf',cm) * gamma_k)
-        case( 2 ) ! saturated_Voce_hard_stress
+        case( enum_hardening_Voce ) ! saturated_Voce_hard_stress
          get_intVar_alpha = alpha_n + sqrt(2./3.) * gamma_k
-        case( 3 ) ! saturated_Miehe_hard_stress
+        case( enum_hardening_linExp ) ! saturated_Miehe_hard_stress
          get_intVar_alpha = alpha_n + sqrt(2./3.) * gamma_k
-        case( 4 ) ! Swift exponential hardening equations as in LS-Dyna (Manual Vol.II, *MAT_122_3D)
+        case( enum_hardening_Swift ) ! Swift exponential hardening equations as in LS-Dyna (Manual Vol.II, *MAT_122_3D)
          get_intVar_alpha = alpha_n + sqrt(2./3.) * gamma_k
-        case( 5 ) ! exponent and exponential
+        case( enum_hardening_potExp ) ! exponent and exponential
          get_intVar_alpha = alpha_n + sqrt(2./3.) * gamma_k
         case default
           write( *, * ) 'elpl-module-get_intVar_alpha<< 
