c5b50fc13fa7c7c31f3c0ba59b50c72b1d097fa6
jfriedlein <Johannes.Friedlein@fau.de>
2021-10-22 16:38:20 +0200

    declared as deprecated, replaced by "get_evolution_dir_n(*)"

--- elasto_plasticity_modules/update_direction_n_n1.f
+++ elasto_plasticity_modules/update_direction_n_n1.f
@@ -12,4 +12,8 @@ c
       update_direction_n_n1 = 1. / get_yielding_norm( stress, HillT_H )
      &                        *  (HillT_H .ddot. stress)
 c      
+      write(*,*) "update_direction_n_n1<< 
+     & OoO use get_evolution_dir_n(*) instead"
+      stop
+c
       end function update_direction_n_n1
