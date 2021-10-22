f085808e06caffea8a49b3130eb63dcc6e209bc7
jfriedlein <Johannes.Friedlein@fau.de>
2021-10-22 16:33:32 +0200

    outsourced N_four into separate function, only implemented for isotropy(?) and Hill48

---
+++ elasto_plasticity_modules/get_N_four.f
@@ -0,0 +1,32 @@
+c
+c
+c
+      type(Tensor4) function get_N_four( stress, HillT_H, cm )
+c
+        use Tensor
+        use cm_manager
+c
+        implicit none
+c
+        type(Tensor2) :: stress
+        type(Tensor4) :: HillT_H
+        real, dimension(*), intent(in) :: cm
+        integer :: anisotropy_type
+c        
+        anisotropy_type = int(cm_get('anisotropy______',cm))
+c
+        if ( anisotropy_type == enum_P_iso
+     &      .OR.
+     &      anisotropy_type == enum_P_aniso_Hill48 ) then
+         get_N_four = ( stress ** HillT_H ** stress)**(-1.5)
+     &                * ( HillT_H * ( stress ** HillT_H ** stress )
+     &                    - ((HillT_H**stress).dya.(stress**HillT_H)) )
+        else
+         write(*,*) 'get_N_four<<
+     &N_four not implemented for the given anisotropy type',
+     &anisotropy_type
+         stop
+       endif
+c
+      end function get_N_four
+      
