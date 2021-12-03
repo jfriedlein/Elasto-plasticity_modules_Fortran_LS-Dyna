fd2c02431268465503381a47c5f07bc37308fb78
jfriedlein <Johannes.Friedlein@fau.de>
2021-12-03 17:07:33 +0100

    updated

--- elasto_plasticity_modules/get_N_four.f
+++ elasto_plasticity_modules/get_N_four.f
@@ -1,7 +1,7 @@
 c
 c
 c
-      type(Tensor4) function get_N_four( stress, HillT_H, cm )
+      type(Tensor4) function get_N_four( stress, HillT_H, cm_all )
 c
         use Tensor
         use cm_manager
@@ -10,14 +10,14 @@ c
 c
         type(Tensor2) :: stress
         type(Tensor4) :: HillT_H
-        real, dimension(*), intent(in) :: cm
+        real, dimension(2,*), intent(in) :: cm_all
         integer :: anisotropy_type
 c        
-        anisotropy_type = int(cm_get('anisotropy______',cm))
+        anisotropy_type = int(cm_get_pair('anisotropy______',cm_all))
 c
         if ( anisotropy_type == enum_P_iso
      &      .OR.
-     &      anisotropy_type == enum_P_aniso_Hill48 ) then
+     &      floor(anisotropy_type/10.) == enum_P_aniso_Hill ) then
          get_N_four = ( stress ** HillT_H ** stress)**(-1.5)
      &                * ( HillT_H * ( stress ** HillT_H ** stress )
      &                    - ((HillT_H**stress).dya.(stress**HillT_H)) )
