9c795c3658949b1120a2b832b6b090256c472002
jfriedlein <Johannes.Friedlein@fau.de>
2021-12-03 17:07:27 +0100

    updated

--- elasto_plasticity_modules/get_intVar_alpha.f
+++ elasto_plasticity_modules/get_intVar_alpha.f
@@ -1,8 +1,8 @@
 c
 c
 c
-      double precision function get_intVar_alpha( alpha_n, gamma_k,
-     & hardening_type, cm )
+      real(kind=8) function get_intVar_alpha( alpha_n, gamma_k,
+     &  cm_all )
 c
       use Tensor
       use cm_manager
@@ -10,7 +10,7 @@ c
 c
       double precision alpha_n
       real gamma_k
-      dimension cm(*)
+      dimension cm_all(2,*)
       integer :: hardening_type
 c Material parameters
       select case( hardening_type )
@@ -19,8 +19,9 @@ c Material parameters
         case( enum_hardening_saturatedAlpha ) ! saturated alpha
          get_intVar_alpha = (alpha_n + sqrt(2./3.) * gamma_k)
      &                     / ( 1. + sqrt(2./3.)
-     &                         * cm_get('hardMod_K_______',cm)
-     &                         /cm_get('hardStress_R_inf',cm) * gamma_k)
+     &                         * cm_get_pair('hardMod_K_______',cm_all)
+     &                         / cm_get_pair('hardStress_R_inf',cm_all)
+     &                           * gamma_k)
         case( enum_hardening_Voce ) ! saturated_Voce_hard_stress
          get_intVar_alpha = alpha_n + sqrt(2./3.) * gamma_k
         case( enum_hardening_linExp ) ! saturated_Miehe_hard_stress
@@ -28,6 +29,10 @@ c Material parameters
         case( enum_hardening_Swift ) ! Swift exponential hardening equations as in LS-Dyna (Manual Vol.II, *MAT_122_3D)
          get_intVar_alpha = alpha_n + sqrt(2./3.) * gamma_k
         case( enum_hardening_potExp ) ! exponent and exponential
+         get_intVar_alpha = alpha_n + sqrt(2./3.) * gamma_k
+        case( enum_hardening_linExpExp )
+         get_intVar_alpha = alpha_n + sqrt(2./3.) * gamma_k
+        case( enum_hardening_loadCurve )
          get_intVar_alpha = alpha_n + sqrt(2./3.) * gamma_k
         case default
           write( *, * ) 'elpl-module-get_intVar_alpha<< 
