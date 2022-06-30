03a0147e0a3eef569098c74e3faf8231e15df65d
jfriedlein <Johannes.Friedlein@fau.de>
2022-06-30 16:38:34 +0200

    changed size of kinematic hardening releated variables to allocatable (size 1,2,3), outsourced extraction of kinHard parameters and using the init function, generalised the computations for dB_dgamma for n_back_stresses

--- elasto_plasticity_modules/get_dB_dgamma.f
+++ elasto_plasticity_modules/get_dB_dgamma.f
@@ -9,53 +9,41 @@ c
       implicit none
 c
       type(Tensor2), intent(in) :: n_k
-      type(Tensor2), dimension(3) :: B_i_n
+      type(Tensor2), dimension(:), allocatable :: B_i_n
       real*8, intent(in) :: gamma_k
       real*8, dimension(*), intent(in) :: hsv
       real*8, dimension(2,*), intent(in) :: cm_all
       real*8 :: a_omega
-      real*8, dimension(3) :: k_1_i, k_2_i
-      integer n_back_stresses
-c Material parameters
-      n_back_stresses = cm_get_pair('n_back_stresses_',cm_all)
-      k_1_i(1) = cm_get_pair('backStr1_K1_____',cm_all) !2666.6667!5000.!77000
-      k_1_i(2) = cm_get_pair('backStr2_K1_____',cm_all)
-      k_2_i(1) = cm_get_pair('backStr1_K2_____',cm_all) !100.!409.75
-      k_2_i(2) = cm_get_pair('backStr2_K2_____',cm_all)
-      if (n_back_stresses==3) then
-            k_1_i(3) = cm_get_pair('backStr3_K1_____',cm_all)
-            k_2_i(3) = cm_get_pair('backStr3_K2_____',cm_all)
-      else
-            k_1_i(3) = 0.
-            k_2_i(3) = 0.
-      endif
-c history
-      B_i_n(1) = hsv_get_symTen2('B_1__', hsv)
-      B_i_n(2) = hsv_get_symTen2('B_2__', hsv)
-      if (n_back_stresses==3) then
-         B_i_n(3) = hsv_get_symTen2('B_3__', hsv)
-      else
-         B_i_n(3) = 0.
-      endif
+      real*8, dimension(:), allocatable :: k_1_i, k_2_i
+      integer hardening_kinematic, n_back_stresses
+      integer i
+c Material parameters and history
+       hardening_kinematic = INT(cm_get_pair('kin_hard_type___',cm_all))
+       call init_kinematic_hardening(
+     &                           !input->
+     &                            hardening_kinematic, cm_all, hsv,
+     &                           !output->
+     &                            B_i_n, n_back_stresses, k_1_i, k_2_i )  
 c
-      a_omega = k_1_i(1) / (1.+k_2_i(1)*gamma_k)
-     &          + k_1_i(2) / (1.+k_2_i(2)*gamma_k)
-     &          + k_1_i(3) / (1.+k_2_i(3)*gamma_k)
+      a_omega=0.
+      do i=1,n_back_stresses
+            a_omega = a_omega + k_1_i(i) / (1.+k_2_i(i)*gamma_k)
+      enddo
 c
-      get_dB_dgamma = (- k_2_i(1) / (1.+k_2_i(1)*gamma_k)**2) * B_i_n(1)
-     &              + (- k_2_i(2) / (1.+k_2_i(2)*gamma_k)**2) * B_i_n(2)
-     &              + (- k_2_i(3) / (1.+k_2_i(3)*gamma_k)**2) * B_i_n(3)
-     &              + n_k * (
-     &                          a_omega
-     &                          + gamma_k * (
-     &                                        k_1_i(1)*k_2_i(1)
-     &                                        / (1.+k_2_i(1)*gamma_k)**2
-     &                                        + k_1_i(2)*k_2_i(2)
-     &                                        / (1.+k_2_i(2)*gamma_k)**2
-     &                                        + k_1_i(3)*k_2_i(3)
-     &                                        / (1.+k_2_i(3)*gamma_k)**2
-     &                                       )
-     &                       )
-c      
+      get_dB_dgamma = 0.
+      do i=1,n_back_stresses
+            get_dB_dgamma = get_dB_dgamma
+     &                      + (- k_2_i(i) / (1.+k_2_i(i)*gamma_k)**2)
+     &                        * B_i_n(i)
+      enddo
+c
+      get_dB_dgamma = get_dB_dgamma + n_k * a_omega
+c
+      do i=1,n_back_stresses
+            get_dB_dgamma = get_dB_dgamma
+     &                      + n_k * gamma_k
+     &                        * k_1_i(i)*k_2_i(i)
+     &                          / (1.+k_2_i(i)*gamma_k)**2
+      enddo
 c
       end function get_dB_dgamma
